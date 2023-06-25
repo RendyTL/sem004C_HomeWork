1671a98905af668e22c5a4bba737d5ad0d8c0f37
Sofia <lucifersatandf@gmail.com>
2023-06-25 13:53:45 +0300

    .

---
+++ C#001/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+
+Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int numberC = numberA;
+
+for(int i = 1; i < numberB; i++ )
+{
+    numberC = numberC * numberA;
+}
+
+Console.Write(numberC);
---
+++ C#002/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+int ReadInt(string text)
+{
+    System.Console.WriteLine(text);
+    return Math.Abs(Convert.ToInt32(Console.ReadLine()));
+}
+
+
+int SumNumbers(int number)
+{
+    int remainder = 0;
+    int result = 0;
+    while (number > 0)
+    {
+        remainder = number % 10;
+        number /= 10;
+        result += remainder;
+    }
+    return result;
+}
+
+int number = ReadInt("Введите число: ");
+System.Console.WriteLine($"Сумма цифр в заданном числе равна: {SumNumbers(number)}");
